1e4e07edeffc809944b1ba3958fe42718e82400b
Maximilien Musquer <mmusquer@f2r9s10.clusters.42paris.fr>
2025-12-22 19:06:33 +0100

    pipex

--- pipex/pipex.c
+++ pipex/pipex.c
@@ -6,36 +6,61 @@
 /*   By: mmusquer <mmusquer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/15 11:47:40 by mmusquer          #+#    #+#             */
-/*   Updated: 2025/12/16 17:05:16 by mmusquer         ###   ########.fr       */
+/*   Updated: 2025/12/22 19:03:43 by mmusquer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
+void	setup(int ac, char **av, int *files, int fd[2])
+{
+	if (ac != 5)
+		error_end("Usage : infile cmd1 cmd2 outfile");
+	files[0] = open_fd_infile(av[1], O_RDONLY);
+	if (files[0] == -1)
+	{
+		perror("No infile");
+		files[0] = open("/dev/null", O_RDONLY);
+	}
+	files[1] = open_fd_outfile(av[4], O_CREAT | O_WRONLY | O_TRUNC);
+	if (files[1] == -1)
+		error_end("Cannot open outfile");
+	if (pipe(fd) == -1)
+		error_end("error pipe");
+}
+
+void	run_child(int fd[2], int *files, char *av, char **envp)
+{
+	pid_t	pid1;
+	pid_t	pid2;
+
+	pid1 = fork();
+	if (pid1 < 0)
+		error_end("error fork child1");
+	if (pid1 == 0)
+		child1(fd, files[0], av[2], envp);
+	pid2 = fork();
+	if (pid2 < 0)
+		error_end("error fork child2");
+	if (pid2 == 0)
+		child2(fd, files[1], av[3], envp);
+	close_fd(fd);
+	close(files[0]);
+	close(files[1]);
+	if (waitpid(pid1, NULL, 0) < 0)
+		perror("waitpid pid1");
+	if (waitpid(pid2, NULL, 0) < 0)
+		perror("waitpid pid2");
+}
+
 int	main(int ac, char **av, char **envp)
 {
-	int fd[2];
-	pid_t pid1;
-	pid_t pid2;
-	int infile;
-	int outfile;
+	int	files[2];
+	int	fd[2];
 
-	if (ac != 5)
+	if (ac != 4)
 		error_end("Pipe use : infile cmd1 | cmd2 outfile");
-	infile = open_fd(av[1], O_RDONLY);
-	outfile = open_fd(av[4], O_CREAT | O_WRONLY | O_TRUNC);
-	if (!pipe(fd))
-		error_end("Error pipe");
-	pid1 = fork();
-	if (pid1 == 0)
-		child1(fd, infile, av[3], envp);
-	pid2 = fork();
-	if (pid2 == 0)
-		child2(fd, outfile, av[3], envp);
-	close_fd(fd);
-	close(infile);
-	close(outfile);
-	waitpid(pid1, NULL, 0);
-	waitpid(pid2, NULL, 0);
+	setup(ac, av, files, fd);
+	run_child(fd, files, av, envp);
 	return (0);
 }
--- pipex/pipex_bonus.h
+++ pipex/pipex_bonus.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pipex.h                                            :+:      :+:    :+:   */
+/*   pipex_bonus.h                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mmusquer <mmusquer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/15 11:48:01 by mmusquer          #+#    #+#             */
-/*   Updated: 2025/12/22 17:35:09 by mmusquer         ###   ########.fr       */
+/*   Updated: 2025/12/22 18:14:38 by mmusquer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PIPEX_H
-# define PIPEX_H
+#ifndef PIPEX__BONUS_H
+# define PIPEX__BONUS_H
 
 # include "libft/libft.h"
 # include <fcntl.h>
@@ -27,8 +27,8 @@ char	*get_path(char **envp);
 char	*find_cmd_path(char *cmd, char **envp);
 void	exec_cmd(char *cmd, char **envp);
 
-void	child1(int *fd, int infiles, char *cmd, char **envp);
-void	child2(int *fd, int outfiles, char *cmd, char **envp);
+void	child(int infile, int oufiles, char *cmd, char **envp);
+void	exec_child(int ac, char **av, char **envp, int infile, int outfile);
 
 void	error_end(char *msg);
 void	free_tab(char **tab);
